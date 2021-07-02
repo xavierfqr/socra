75d012f03235f7b386adb7037e82fcfc69f5e6f3
remydao <remy.dao.pro@gmail.com>
2021-07-02 17:25:50 +0200

    Add: Layered architecture and router logic

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/socra", { useUnifiedTopology: true,  useNewUrlParser: true });
 var taskSchema = require("./models/taskSchema");
 
+app.use(require('./api/add'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
-
