a5349a0fa87a808d66d4f1690dbec164bc3bc3ed
remydao <remy.dao.pro@gmail.com>
2021-07-02 17:08:58 +0200

    Add: mongoose stack and first schema

---
+++ models/taskSchema.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var taskSchema = new Schema({
+    location: String,
+    duration: { type: Number, min: 0 },
+    price: { type: Number, min: 0 },
+    remote: { type: Number, min: 0, max: 100 },
+    start: String,
+    job: String,
+    context: String,
+    mission: String,
+});
+
+module.exports = {
+    taskSchema: taskSchema
+};
