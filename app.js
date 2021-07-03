e88daadd3dec3b241864c01d1165da8cb16035e5
Xavier <facqueur.xavier@gmail.com>
2021-07-03 19:44:20 +0200

    Add: Swagger dependencies and configuration

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocs = require('./swagger.json');
+
+const mongoose = require('mongoose');
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 mongoose.connect("mongodb://localhost:27017/socra", { useUnifiedTopology: true,  useNewUrlParser: true });
 
 // To parse req.body
