512b991eb71534cf1323dd91a0737bb587ccbb5b
David Ghiassi <david.ghiassi@gmail.com>
2021-07-04 13:46:52 +0200

    Add: test for first POST endpoint

--- test/task.js
+++ test/task.js
@@ -19,14 +19,43 @@ describe('Tasks', () => {
     });
 
     // Test: GET all the tasks
-    describe('/GET task', () => {
+    describe('/GET tasks', () => {
         it('Should GET all the tasks', (done) => {
             chai.request(server)
-                .get('/task')
+                .get('/tasks')
                 .end((err, res) => {
                     res.should.have.status(200); // Status code
                     res.body.should.be.a('array'); // []
                     res.body.length.should.be.eql(0); // no task
+                done();
+            });
+        });
+    });
+
+    describe('/POST task', () => {
+        it('Should POST a task', (done) => {
+            let task = {
+                location: "Issy-les-Moulineaux",
+                duration: 12,
+                remote: 100,
+                start: "ASAP",
+                job: "développeur ReactJs",
+                context: "intégration au sein de l’équipe Engineering du pôle Industrialisation, Cloud and Data, le consultant contribuera aux activités APIs et API Management",
+                mission: "Participer aux Comités d’Architecture pour garantir la bonne conformité des bonne pratique des APIs. Promouvoir les pratiques API First au sein du groupe. Rédiger / Maintenir un Guideline de développement d’API (création de modèle d’API, ...)",
+            }
+            chai.request(server)
+                .post('/task')
+                .send(task)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('location');
+                    res.body.should.have.property('duration');
+                    res.body.should.have.property('remote');
+                    res.body.should.have.property('start');
+                    res.body.should.have.property('job');
+                    res.body.should.have.property('context');
+                    res.body.should.have.property('mission');
                 done();
             });
         });
