2f0663781b79ba5aca5da31b8883f151ad644023
Xavier <facqueur.xavier@gmail.com>
2021-07-07 20:46:39 +0200

    Add: Unit test - Post task with fail (multiple invalid fields)

--- test/post-test.js
+++ test/post-test.js
@@ -50,7 +50,7 @@ describe('Post Endpoint Tests', () => {
 
     // Test: Post one task with fail (error 400) because of an invalid task field
     describe('Post one task fail', () => {
-        it('Should not POST a task', (done) => {
+        it('Should not POST a task with one wrong field', (done) => {
             let task = {
                 location: "Issy-les-Moulineaux",
                 duration: -12,  //'duration' field should NOT be negative
@@ -71,6 +71,35 @@ describe('Post Endpoint Tests', () => {
                     res.body.should.have.property('error');
                     res.body.should.have.property('fields');
                     res.body.should.have.property('messages');
+                done();
+            });
+        });
+    });
+
+    // Test: Post one task with fail (error 400) because of several invalid task fields
+    describe('Post one task fail', () => {
+        it('Should not POST a task with multiple wrong fields', (done) => {
+            let task = {
+                location: "Issy-les-Moulineaux",
+                duration: -12,  //'duration' field should NOT be negative
+                remote: -100,   //'remote' field should NOT be negative
+                start: "ASAP",
+                job: "développeur ReactJs",
+                context: "intégration au sein de l’équipe Engineering du pôle Industrialisation, Cloud and Data, le consultant contribuera aux activités APIs et API Management",
+                mission: "Participer aux Comités d’Architecture pour garantir la bonne conformité des bonne pratique des APIs. Promouvoir les pratiques API First au sein du groupe. Rédiger / Maintenir un Guideline de développement d’API (création de modèle d’API, ...)",
+                timestamp: "Pas une date"
+            }
+            chai.request(server)
+                .post('/task')
+                .send(task)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.error.should.be.eql('Bad request, some fields are not well formatted. Please read the documentation.') //check error message from response
+                    res.body.should.have.property('error');
+                    res.body.should.have.property('fields');
+                    res.body.should.have.property('messages');
+                    res.body.fields.length.should.be.above(1);  //check if there are more than one field wrong
                 done();
             });
         });
