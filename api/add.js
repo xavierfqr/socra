7b67816151ff7ef1a2872139f6ef4ca61b577e7a
remydao <remy.dao.pro@gmail.com>
2021-07-07 19:30:58 +0200

    Add: personnalized error 400 Bad Request handling

--- api/add.js
+++ api/add.js
@@ -1,22 +1,31 @@
 const express = require('express');
+const handleValidationError = require('../utils/handle-errors')
 const router = express.Router();
 const TaskModel = require("../models/taskModel");
 
 // Add a task to the database
 router.post('/task', async function(req, res) {
-    const task = new TaskModel({
-        location: req.body.location,
-        duration: req.body.duration,
-        price: req.body.price,
-        remote: req.body.remote,
-        start: req.body.start,
-        job: req.body.job,
-        context: req.body.context,
-        mission: req.body.mission,
-        timestamp: Date.now(),
-    })
-    await task.save();
-    res.send(task);
+    try {
+        const task = new TaskModel({
+            location: req.body.location,
+            duration: req.body.duration,
+            price: req.body.price,
+            remote: req.body.remote,
+            start: req.body.start,
+            job: req.body.job,
+            context: req.body.context,
+            mission: req.body.mission,
+            timestamp: Date.now(),
+        });
+        await task.save();
+        res.send(task);
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            return error = handleValidationError(error, res);
+        } else {
+            res.status(500).send({error: "An unknown error has occured."})
+        }
+    }
 });
 
 module.exports = router;
