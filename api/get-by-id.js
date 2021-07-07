cf1c97f9c64f44098edf86cce4ae0aee222de24a
David Ghiassi <david.ghiassi@gmail.com>
2021-07-07 22:16:17 +0200

    Add: test for PATCH

--- api/get-by-id.js
+++ api/get-by-id.js
@@ -4,8 +4,8 @@ const TaskModel = require("../models/task-model");
 
 // Get by id
 router.get('/tasks/:id', async function(req, res) {
-    const tasks = await TaskModel.findById(req.params.id).exec();
-	res.send(tasks);
+    const task = await TaskModel.findById(req.params.id).exec();
+	res.send(task);
 });
 
 module.exports = router;
