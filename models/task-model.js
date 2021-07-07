d4b24a9df3ca6eeb31fa3177ae76861220816a20
David Ghiassi <david.ghiassi@gmail.com>
2021-07-07 17:31:56 +0200

    Fix: renamed files

---
+++ models/task-model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var taskSchema = new Schema({
+    location: String,
+    duration: { type: Number, min: 0 },
+    price: { type: Number, min: 0 },
+    remote: { type: Number, min: 0, max: 100 },
+    start: String,
+    job: String,
+    context: String,
+    mission: String,
+    timestamp: Number,
+});
+
+module.exports = mongoose.model("taskModel", taskSchema);
