96a1c0636f402d64806034674ad86c9ebe6bc2b7
Xavier <facqueur.xavier@gmail.com>
2021-07-08 12:13:15 +0200

    Fix: get Pdf callback function written in controllers

--- api/pdf.js
+++ api/pdf.js
@@ -1,28 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const generatePDF = require('../services/pdf-generator')
-const TaskModel = require("../models/task-model");
-const mongoose = require("mongoose");
+const {getPdfTask} = require('../controllers/task-controller');
 
-router.get('/pdf/:id', async function(req, res) {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-        return res.status(400).send({error: "The provided ID is not a valid mongoose ID"});
-    }
-    const taskInfo = await TaskModel.findById(req.params.id, 'location duration price remote start job context mission').exec();
+router.get('/pdf/:id', getPdfTask);
 
-    if (!taskInfo) {
-        return res.status(404).send({error: "This task does not exists. Id: " + req.params.id + ' does not exist'});
-    }
-
-    const filename = 'taskInfo';
-    const pdfStream = await generatePDF(filename, taskInfo.toObject());
-
-    res.writeHead(200, {
-        'Content-Length': Buffer.byteLength(pdfStream),
-        'Content-Type': 'application/pdf',
-        'Content-disposition': 'attachment;filename=' + filename + '.pdf',
-      })
-      .end(pdfStream);
-});
 
 module.exports = router;
--- controllers/task-controller.js
+++ controllers/task-controller.js
@@ -2,6 +2,8 @@ const TaskModel = require("../models/task-model");
 const handleValidationError = require('../utils/handle-errors');
 var mongoose = require('mongoose');
 const orderTasksByKeywords = require("../services/search-service");
+const generatePDF = require('../services/pdf-generator')
+
 
 
 const addTask = async (req, res) => {
@@ -81,10 +83,32 @@ const searchTasks = async (req, res) => {
     }
 }
 
+const getPdfTask = async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send({error: "The provided ID is not a valid mongoose ID"});
+    }
+    const taskInfo = await TaskModel.findById(req.params.id, 'location duration price remote start job context mission').exec();
+
+    if (!taskInfo) {
+        return res.status(404).send({error: "This task does not exists. Id: " + req.params.id + ' does not exist'});
+    }
+
+    const filename = 'taskInfo';
+    const pdfStream = await generatePDF(filename, taskInfo.toObject());
+
+    res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(pdfStream),
+        'Content-Type': 'application/pdf',
+        'Content-disposition': 'attachment;filename=' + filename + '.pdf',
+      })
+      .end(pdfStream);
+}
+
 module.exports = {
     addTask,
     getTaskById,
     getTasks,
     modifyTaskById,
-    searchTasks
+    searchTasks,
+    getPdfTask
 }
