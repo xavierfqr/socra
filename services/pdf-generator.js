cbf719f24f1a1c2899252eef9d329366e07c06cc
remydao <remy.dao.pro@gmail.com>
2021-07-08 12:02:37 +0200

    Add: Style to pdf

--- services/pdf-generator.js
+++ services/pdf-generator.js
@@ -5,14 +5,24 @@ const fs = require('fs');
 async function generatePDF(filename, taskInfo){
     try {
         const doc = new PDFDocument();
+        doc.fontSize(20);
+        doc.font('Helvetica-Bold').text('Task description', {
+            align: 'center'
+        });
+        doc.moveDown();
+        doc.moveDown();
+
+
         doc.fontSize(12);
-        
         for (var key in taskInfo) {
-            var val = taskInfo[key];
-            doc.font('Helvetica-Bold').text(key + ': ', {
-                continued: true
-            }).font('Helvetica').text(val);
-            doc.moveDown();
+            if (key !== "_id") {
+                var val = taskInfo[key];
+                var cap = key.substring(0, 1).toUpperCase() + key.substring(1);
+                doc.font('Helvetica-Bold').text(cap + ': ', {
+                    continued: true
+                }).font('Helvetica').text(formatDescription(key, val));
+                doc.moveDown();
+            }
         };
 
         doc.pipe(fs.createWriteStream(`${__dirname}/../` + filename + '.pdf'));
@@ -24,6 +34,19 @@ async function generatePDF(filename, taskInfo){
         return pdfStream;
     } catch (error) {
         return null;
+    }
+}
+
+function formatDescription(key, value) {
+    switch (key) {
+        case 'duration':
+            return value + " month(s)";
+        case 'price':
+            return value + " euros";
+        case 'remote':
+            return value + "%";
+        default:
+            return value;
     }
 }
 
